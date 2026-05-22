c8f13677f95aa04639ad709acb71e41b31aad1db
Steven Pajaro <stevenpajaro27@gmail.com>
2026-05-22 10:23:30 -0500

    feat: agregar consulta personalizada para buscar DetalleVentas por producto_id

--- backend/src/main/java/com/proyecto/seged/repository/DetalleVentasRepository.java
+++ backend/src/main/java/com/proyecto/seged/repository/DetalleVentasRepository.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.Query;
+
 @Repository
 public interface DetalleVentasRepository extends MongoRepository<DetalleVentas, String> {
 
+    @Query("{ 'producto_id' : ?0 }")
     List<DetalleVentas> findByProducto_id(String productoId);
 }
